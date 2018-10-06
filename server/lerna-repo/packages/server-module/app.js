1ddae53c6d12f27fdb73024bad3e3a000fc4e752
Gnurt <huavitrung16@gmail.com>
2018-10-06 16:23:17 +0700

    commit to fix bug can't get router

--- server/lerna-repo/packages/server-module/app.js
+++ server/lerna-repo/packages/server-module/app.js
@@ -1,6 +1,10 @@
 
 const accountRoute = require('admin-module');
-
+const transactionRoute=require('transaction-module');
 module.exports = function(app) {
     app.use('/secure',accountRoute);
+}
+
+module.exports = function(app) {
+    app.use('/transaction',transactionRoute);
 }
