bce9dc50b54a20f902c531cea042955c87814820
LE Xuan Tien <lexuantien7617@outlook.com.vn>
2018-10-17 16:26:46 +0700

    Add: tabbar interaction

--- mobile/test/App.js
+++ mobile/test/App.js
@@ -8,6 +8,9 @@ YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated']);
 
 import { Provider } from  "react-redux";
 import store from './src/redux/store';
+
+import TabBar from './src/components/custom-ui/tabbar-interaction/TabBar';
+
 export default class App extends Component {
 
 
@@ -24,11 +27,44 @@ export default class App extends Component {
     const { splashScreen } = this.state;
     let mainScreen = splashScreen ? <Splash /> : <RootNavigator />;
 
-    return (
-      <Provider store = { store } >
-        <RootNavigator/>
-      </Provider>
-    );
+    // return (
+    //   <Provider store = { store } >
+    //     {/* <RootNavigator/> */}
+        
+    //   </Provider>
+    // );
+    return (<TabBar>
+      <TabBar.Item
+      // icon={require('./tab1.png')}
+      // selectedIcon={require('./tab1_sel.png')}
+       title="Tab1"
+       screenBackgroundColor={{ backgroundColor: '#008080' }}
+   >
+      <View>
+
+      </View>
+      </TabBar.Item>
+      <TabBar.Item
+      // icon={require('./tab2.png')}
+       // selectedIcon={require('./tab2_sel.png')}
+       title="Tab2"
+       screenBackgroundColor={{ backgroundColor: '#F08080' }}
+   >
+      <View>
+
+      </View>
+      </TabBar.Item>
+      <TabBar.Item
+       //icon={require('./tab3.png')}
+       //selectedIcon={require('./tab3_sel.png')}
+       title="Tab3"
+       screenBackgroundColor={{ backgroundColor: '#485d72' }}
+   >
+       <View>
+
+       </View>
+       </TabBar.Item>
+       </TabBar>);
   }
 }
 
