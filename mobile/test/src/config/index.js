7b33a117195351f30373c5211f9baeb3bc7f0bb4
Gnurt <huavitrung16@gmail.com>
2018-12-06 00:01:12 +0700

    init firebase

--- mobile/test/src/config/index.js
+++ mobile/test/src/config/index.js
@@ -1,6 +1,19 @@
+import * as firebase from 'firebase';
+
 const GLOBAL = {
-  HostName: "http://192.168.1.3:8080" // config host  here [aws, heroku, localhost(use Ipv4) , ...]
+  HostName: "http://192.168.1.2:8080" // config host  here [aws, heroku, localhost(use Ipv4) , ...]
 
 };
+const firebaseConfig = {
+  apiKey: "AIzaSyC6yozzmREqGVzutPzPp9fXgEC_FOZGjdc",
+  authDomain: "online-payment-ac4b8.firebaseapp.com",
+  databaseURL: "https://online-payment-ac4b8.firebaseio.com",
+  storageBucket: "online-payment-ac4b8.appspot.com",
+};
 
-export default GLOBAL;
+firebase.initializeApp(config);
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const provider = new firebase.auth.FacebookAuthProvider();
+export const storage = firebase.storage();
+export default {GLOBAL};
